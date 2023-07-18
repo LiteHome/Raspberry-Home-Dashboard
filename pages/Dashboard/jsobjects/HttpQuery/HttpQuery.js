03be2d562ae32229adbad8349c14b2537e91ea6b
noPepsi needCola <timnopepsi@gmail.com>
2023-07-18 16:49:52 +0000

    两个界面都完成定时器刷新界面数据

--- pages/Dashboard/jsobjects/HttpQuery/HttpQuery.js
+++ pages/Dashboard/jsobjects/HttpQuery/HttpQuery.js
@@ -6,18 +6,6 @@ export default {
 		setInterval(this.httpQuery, this.interval * this.sec, "HttpQuery")
 		showAlert("开始拉取信息, 目前间隔是 " + this.interval + "s")
 	},
-	stopTimer: () => {
-		clearInterval("HttpQuery")
-		showAlert("停止拉取信息")
-	},
-
-	changeTimer: () => {
-		if(switch_for_data_pull.isSwitchedOn){
-			 this.startTimer()
-		} else {
-			 this.stopTimer()
-		}
-	},
 	httpQuery: () => {
 			get_all_boards.run().then(() => {boards_amount.setText(String(get_all_boards.data.length))}).catch(() => boards_amount.setText("0"))
 			get_all_gateways.run().then(() => {gateways_amount.setText(String(get_all_gateways.data.length))}).catch(() => gateways_amount.setText("0"))
--- pages/DeviceManage/jsobjects/Http_Query/Http_Query.js
+++ pages/DeviceManage/jsobjects/Http_Query/Http_Query.js
@@ -1,4 +1,8 @@
 export default {
+	sec:60,
+	unit:1000,
+	timerName: "httpQuery",
+
 	getAllDevices: () => {
 		return get_all_devices.data.map((row) => {
 			return {
@@ -12,25 +16,64 @@ export default {
 		})
 	},
 	setDeviceName: async (deviceName, deviceUuid) => {
-
+		// 尝试获取锁
+		if(!device_list.isVisible){
+			showAlert("列表正在更新, 请稍后再试")
+			return 1
+		}
+		// 抢占锁
 		await device_list.setVisibility(false)
+		// 发送请求, 失败则必须释放锁
 		try{
 			await set_devices_name.run({"device_name":deviceName, "device_uuid":deviceUuid})
+			showAlert("完成修改")
 		}catch(error){
-			await device_list.setVisibility(true)
 			showAlert("无法更新设备名称")
-			return
+			await device_list.setVisibility(true)
+			return 1
 		}
 
+		// 数据库修改后更新界面
 		try{
 			await get_all_devices.run()
+			showAlert("刷新界面")
 		}catch(error){
-			await device_list.setVisibility(true)
 			showAlert("无法获取最新设备列表")
-			return
+		}finally{
+			await device_list.setVisibility(true)
 		}
+	},
+	refreshTable:async () => {
+		// 尝试获取锁
+		if(!device_list.isVisible){
+			showAlert("正在提交信息到数据库,取消刷新界面")
+			return 1
+		}
+		// 抢占锁
+		await device_list.setVisibility(false)
+		try{
+			await get_all_devices.run()
+			showAlert("完成更新")
+		}catch(error){
+			showAlert("无法获取最新设备列表")
+		}finally{
+			// 释放锁
+			await device_list.setVisibility(true)
+		}
+	},
+	startTimer: () => {
+		this.stopTimer()
+		setInterval(this.refreshTable, this.sec * this.unit, this.timerName)
+		showAlert("开始拉取信息, 目前间隔是 " + this.sec + "s")
+	},
+	stopTimer: () => {
+		try{
+			clearInterval(this.timerName)
+			showAlert("停止定时器")
+		}catch(error){
 
-		await device_list.setVisibility(true)
-		showAlert("完成更新")
+		}finally{
+			return 1
+		}
 	}
 }
