46825ba42e7282eeb6644e3978fdd763e6845b5f
noPepsi needCola <timnopepsi@gmail.com>
2023-07-19 15:30:09 +0000

    完成在线设备下拉框

--- pages/Dashboard/jsobjects/HttpQuery/HttpQuery.js
+++ pages/Dashboard/jsobjects/HttpQuery/HttpQuery.js
@@ -1,14 +1,38 @@
 export default {
 	sec:1000,
-	interval:15,
+	interval:60,
+	timerName:"timer",
+	sensor1Id:-1,
 
 	startTimer: () => {
-		setInterval(this.httpQuery, this.interval * this.sec, "HttpQuery")
+		setInterval(this.intervalAction, this.interval * this.sec, this.timerName)
 		showAlert("开始拉取信息, 目前间隔是 " + this.interval + "s")
 	},
-	httpQuery: () => {
-			get_all_boards.run().then(() => {boards_amount.setText(String(get_all_boards.data.length))}).catch(() => boards_amount.setText("0"))
-			get_all_gateways.run().then(() => {gateways_amount.setText(String(get_all_gateways.data.length))}).catch(() => gateways_amount.setText("0"))
-			get_all_sensors.run().then(() => {sensors_amount.setText(String(get_all_sensors.data.length))}).catch(() => sensors_amount.setText("0"))
+	intervalAction: () => {
+		this.refreshText()
+		get_all_online_device.run()
+		
+	},
+	refreshText: () => {
+		// 展示控制板, 传感器, 网关的数量
+		get_all_boards.run().then(() => {boards_amount.setText(String(get_all_boards.data.length))}).catch(() => boards_amount.setText("0"))
+		get_all_gateways.run().then(() => {gateways_amount.setText(String(get_all_gateways.data.length))}).catch(() => gateways_amount.setText("0"))
+		get_all_sensors.run().then(() => {sensors_amount.setText(String(get_all_sensors.data.length))}).catch(() => sensors_amount.setText("0"))
+	},
+	showAllOnlineDevice: () => {
+		return get_all_online_device.data.filter((row) => {
+			if(isNaN(row.device_name)){
+				return true
+			}
+		}).map((row) => {
+			return {
+				"label":row.device_name,
+				"value":row.id
+			}
+		})
+	},
+	// todo: 选择传感器后更新数据
+	test: () => {
+		return get_latest_data_by_id.run({"id":50})
 	}
 }
